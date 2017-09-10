f53b3bb21e53be65064091b9995f068bc3358f81
ashwin-pc <ashwinpc1993@gmail.com>
2017-09-11 01:22:08 +0530

    fixed format

--- dependencies/WhatsappSim/WhatsappSimClass.js
+++ dependencies/WhatsappSim/WhatsappSimClass.js
@@ -26,7 +26,7 @@
         this.types = [
             {
                 name: "email",
-                testRegex: /^\d{2}\/\d{2}\/\d{2},\s\d{1,2}:\d{2}\s[AP]M/,
+                testRegex: /^\d{1,2}\/\d{1,2}\/\d{2,4},\s\d{1,2}:\d{2}\s[AP]M/,
                 splitRegx: /(\s-\s|\:\s)/
             }, {
                 name: "copyWeb",
