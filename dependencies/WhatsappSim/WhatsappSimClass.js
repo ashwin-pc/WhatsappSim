7bb2a0c02a5d883090733fa5b00bad92ce316558
ashwin-pc <ashwinpc1993@gmail.com>
2017-11-14 18:29:54 +0530

    added format

--- dependencies/WhatsappSim/WhatsappSimClass.js
+++ dependencies/WhatsappSim/WhatsappSimClass.js
@@ -55,6 +55,11 @@
                 testRegex: /^\[\d{2}\/\d{2}\,\s\d{1,2}:\d{2}\]/,
                 splitRegx: /(\]\s|\:\s)/,
                 hasBracket: true
+            }, {
+                name: "webNew",
+                testRegex: /^\[\d{2}\/\d{2},\s\d{1,2}:\d{2}\]/,
+                splitRegx: /(\]\s|\:\s)/,
+                hasBracket: true
             }
         ]
     }
